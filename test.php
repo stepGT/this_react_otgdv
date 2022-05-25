1695b296c43510131911b1cf76adb107950856e8
stepGT <stepGT@mail.ru>
2022-05-26 00:49:32 +1000

    Deleted unnecessary files

--- test.php
+++
@@ -1,7 +0,0 @@
-
-<?
-   /* if (mail("pr@deviart.ru","test subject", "test body","From: info@unifactoring.ru"))
-    echo "Сообщение передано функции mail, проверьте почту в ящике.";
-    else
-    echo "Функция mail не работает, свяжитесь с администрацией хостинга.";*/
-?>
